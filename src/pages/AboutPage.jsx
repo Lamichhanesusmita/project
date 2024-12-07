a763240dd0ffa290ee6476b159403c5f00327108
lamichhanesusmita <lamichhanesusmita76@gmail.com>
2024-12-07 16:37:16 +0545

    real

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,13 +7,20 @@ const AboutPage = () => {
         <h1>About Us</h1>
         <p>
           {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          quaerat veniam ea id doloribus optio praesentium nihil laboriosam
-          quisquam, magni repellat dolorum tenetur, voluptatibus ducimus ut quae
-          voluptatum, blanditiis natus.Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Aliquam temporibus tempore, totam saepe excepturi
-          ducimus atque est nam molestias animi ut architecto deleniti, quos,
-          voluptas odio placeat dolorum blanditiis id.{" "}
+          It is the aim of Shree Brahmarupa Secondary School to provide K-12 education that every
+          student deserves. We, at Shree Brahmarupa Secondary School, appreciate diversity and
+          realize that each student is different and should be treated as an
+          unique individual. Our constant endeavor is to help each student
+          realize his/her full potential and discover new horizons in learning.
+          We believe in bringing out the best in our students so that they can
+          rise to the top ever empowered by a belief in unlimited human
+          potential. At Shree Brahmarupa Secondary School, creativity is nurtured, with learners
+          being treated as just learners, imbued with a passion for knowledge
+          and discovery and not as rote machines. In terms of overall academic
+          orientation, we place a pronounced emphasis on Science and Technology,
+          English, and Mathematics (STEAM), along with other domains
+          critical to overall academic achievement and sustained academic
+          excellence{" "}
         </p>
       </div>
     </div>
